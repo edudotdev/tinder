29ab8d0bf0ef847d5839c565e380237e80f162d0
adalbertopc <adalbertinho90@gmail.com>
2021-03-15 13:09:15 -0700

    feat: add react-router-dom and .env api_url variable

--- src/App.js
+++ src/App.js
@@ -1,59 +1,60 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
-import Form from './components/Form'
-import Cards from './components/Cards'
+import Form from './components/Form';
+import Cards from './components/Cards';
 
 function App() {
+	//	const [status, setStatus] = useState(0);
+	const [peoples, setPeoples] = useState([]);
+	const [category, setCategory] = useState('');
+	const [search, setSearch] = useState('');
 
-  const [peoples, setPeoples] = useState([])
-  const [category, setCategory] = useState('')
-  const [search, setSearch] = useState('')
+	const apiConnection = (url = `${process.env.REACT_APP_API_URL}/people`) => {
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => setPeoples(data.data))
+			.catch((e) => console.log(e));
+	};
 
-  const apiConnection = (url = 'https://0eaa1292f690.ngrok.io/people') => {
-    fetch(url)
-    .then(response => response.json())
-    .then(data => setPeoples(data))
-  }
+	useEffect(() => {
+		apiConnection();
+	}, []);
 
-  useEffect(() => {
-    apiConnection()
-  }, [])
+	useEffect(() => {
+		fetch(`${process.env.REACT_APP_API_URL}/people?gender=${category}`)
+			.then((response) => response.json())
+			.then((data) => setPeoples(data.data))
+			.catch((error) => console.log(error));
+	}, [category]);
 
-  useEffect(() => {
-    fetch(`https://0eaa1292f690.ngrok.io/people?gender=${category}`)
-    .then((response) => response.json())
-    .then(data => setPeoples(data))
-    .catch(error => console.log(error))
-  }, [category])
+	const refresh = (id) => {
+		setPeoples((peoples) => peoples.filter((people) => people.idPeople !== id));
+	};
 
-  useEffect(() => {
-    if(search) {
-
-    }
-  }, [search])
-
-  const refresh = (id) => {
-    setPeoples(peoples => peoples.filter(people => people.idPeople !== id))
-  }
-
-  return (
-    <>
-      {false
-        ? 
-          <Form />
-        :
-            <Cards 
-              peoples={peoples}
-              setPeoples={setPeoples}
-              setCategory={setCategory}
-              category={category}
-              setSearch={setSearch}
-              search={search}
-              refresh={refresh}
-            />
-      }
-    </>
-  );
+	return (
+		<Router>
+			<div>
+				<Switch>
+					<Route path='/register'>
+						<Form />
+					</Route>
+					<Route path='/'>
+						<Cards
+							peoples={peoples}
+							setPeoples={setPeoples}
+							setCategory={setCategory}
+							category={category}
+							setSearch={setSearch}
+							search={search}
+							refresh={refresh}
+						/>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,60 +1,60 @@
-import React from 'react'
+import React from 'react';
 
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const CardS = styled.div`
-  background: #fff;
-  border-radius: 30px;
-  overflow: hidden;
-  padding: 5px;
-  transition: transform 0.4s ease, box-shadow 0.4s ease;
+	background: #fff;
+	border-radius: 30px;
+	overflow: hidden;
+	padding: 5px;
+	transition: transform 0.4s ease, box-shadow 0.4s ease;
 
-  &:hover {
-    transform: translateY(-10px);
-    box-shadow: 0 20px 30px 1px rgba(0, 0, 0, .04);
-  }
-`
+	&:hover {
+		transform: translateY(-10px);
+		box-shadow: 0 20px 30px 1px rgba(0, 0, 0, 0.04);
+	}
+`;
 
 const ImageWrapper = styled.div`
-  position: relative;
-  overflow: hidden;
-  padding-bottom: 100%;
-  border-radius: 30px;
+	position: relative;
+	overflow: hidden;
+	padding-bottom: 100%;
+	border-radius: 30px;
 
-  img {
-    position: absolute;
-    width: 100%;
-    height: auto;
-  }
-  
-  &:hover button {
-    opacity: 1;
-    pointer-events: unset;
-  }
-`
+	img {
+		position: absolute;
+		width: 100%;
+		height: auto;
+	}
+
+	&:hover button {
+		opacity: 1;
+		pointer-events: unset;
+	}
+`;
 
 const Info = styled.div`
-  padding: 10px;
+	padding: 10px;
 
-  span {
-    font-weight: 600;
-    display: block; 
-    color: #2f2f2f;
-    font-size: 18px;
-  }
+	span {
+		font-weight: 600;
+		display: block;
+		color: #2f2f2f;
+		font-size: 18px;
+	}
 
-  .about {
-    /* padding: 10px; */
-    position: relative;
-    font-size: 14px;
-    font-weight: 600;
-    color: #b0b0b0;
-    border-radius: 30px;
-    padding: 3px 10px;
-    /* margin-left: 35px;
+	.about {
+		/* padding: 10px; */
+		position: relative;
+		font-size: 14px;
+		font-weight: 600;
+		color: #b0b0b0;
+		border-radius: 30px;
+		padding: 3px 10px;
+		/* margin-left: 35px;
     margin-top: 20px; */
 
-    /* &::before {
+		/* &::before {
       content: 'About';
       position: absolute;
       left: -35px;
@@ -65,93 +65,84 @@ const Info = styled.div`
       font-size:10px;
       border-radius: 99px;
     } */
-  }
+	}
 
-  .gender {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-weight: 600;
-    font-size: 12px;
-    padding: 5px 10px;
-    border-radius: 30px;
-    width: 80px;
-    margin: 10px 0;
+	.gender {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-weight: 600;
+		font-size: 12px;
+		padding: 5px 10px;
+		border-radius: 30px;
+		width: 80px;
+		margin: 10px 0;
 
-    span, p {
-      display: inline-block;
-      padding-right: 5px;
-    }
-  }
-`
+		span,
+		p {
+			display: inline-block;
+			padding-right: 5px;
+		}
+	}
+`;
 
 const BtnDelete = styled.button`
-  position: absolute;
-  width: 50px;
-  height: 50px;
-  left: 10px;
-  top: 10px;
-  color: #0867ff;
-  background: #bbcbff;
-  border-radius: 30px;
-  border: none;
-  padding: 10px;
-  z-index: 2;
-  cursor: pointer;
-  opacity: 0;
-  pointer-events: none;
-  transition: opacity 0.3s ease;
+	position: absolute;
+	width: 50px;
+	height: 50px;
+	left: 10px;
+	top: 10px;
+	color: #0867ff;
+	background: #bbcbff;
+	border-radius: 30px;
+	border: none;
+	padding: 10px;
+	z-index: 2;
+	cursor: pointer;
+	opacity: 0;
+	pointer-events: none;
+	transition: opacity 0.3s ease;
 
-  span {
-    pointer-events: none;
-  }
-`
+	span {
+		pointer-events: none;
+	}
+`;
 
-const Card = ({people, setPeoples, refresh}) => {
+const Card = ({ people, setPeoples, refresh }) => {
+	const { idPeople, firstName, lastName, age, gender, preferences, description, image } = people;
 
-  const {idPeople, firstName, lastName, age, gender, preferences, description, image} = people
+	const handleDelete = (e) => {
+		fetch(`${process.env.REACT_APP_API_URL}/people/${idPeople}`, { method: 'DELETE' })
+			.then(() => refresh(idPeople))
+			.catch((error) => console.log(error));
+	};
 
-  const handleDelete = (e) => {
-    fetch(`https://0eaa1292f690.ngrok.io/people/${idPeople}`, {method: 'DELETE'})
-    .then(() => refresh(idPeople))
-    .catch(error => console.log(error))
-  } 
+	return (
+		<CardS>
+			<ImageWrapper>
+				<img src={image} alt='' />
 
-  return ( 
-    <CardS>
-      <ImageWrapper>
-        <img src={image} alt=""/>
+				<BtnDelete onClick={handleDelete}>
+					<span className='material-icons'>delete</span>
+				</BtnDelete>
+			</ImageWrapper>
 
-        <BtnDelete onClick={handleDelete}>
-          <span className="material-icons">
-            delete
-          </span>
+			<Info>
+				<span>
+					{firstName} {lastName}, {age}
+				</span>
 
-        </BtnDelete>
-      </ImageWrapper>
+				<div className={`gender ${gender}`}>
+					<span className='material-icons'>
+						{gender === 'Female' ? 'female' : 'male'}
+					</span>
+					<p>{gender}</p>
+				</div>
 
-      <Info>
-        <span>{firstName} {lastName}, {age}</span>
-     
-        <div className={`gender ${gender}`}>
-          <span className="material-icons">
-            {gender === 'Female' 
-              ? 
-                'female'
-
-              :
-                'male'
-            }
-          </span>
-          <p>{gender}</p>
-        </div>
-
-        <div className="about">
-          {description}
-        </div>
-      </Info>
-    </CardS>
-   );
-}
+				<div className='about'>{description}</div>
+			</Info>
+		</CardS>
+	);
+};
 
 export default Card;
